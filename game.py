40a890976cdeac3b67b83b142d1ed3257c1133e8
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2022-10-23 15:21:00 -0400

    Added the battle phase, now working on a determine winner function. I have a lot of if statements ahead of me

--- game.py
+++ game.py
@@ -1,21 +1,24 @@
 from human import Human
 from ai import AI
-
-class Gesture:
-    def __init__(self) -> None:
-        gesture_list = ['rock','paper','scissors','lizard','spock']
+from random import Random
 
 class Game:
     def __init__(self) -> None:
-        self.human = Human('Player 1')
-        self.human = Human('Player 2')
-        self.ai = AI('AI 1')
-        self.ai = AI('AI 2')
+        self.human_1 = Human('Player 1')
+        self.human_2 = Human('Player 2')
+        self.ai_1 = AI('AI 1')
+        self.ai_2 = AI('AI 2')
+        global gesture_list
+        gesture_list = ['rock','paper','scissors','lizard','spock']
 
 
     def run_game(self):
         self.display_welcome()
         self.display_rules()
+        player_count = self.determine_player_count
+        if player_count == 1:
+            self.battle(self.human_1, self.ai_1)
+
     
     def display_welcome(self):
         print("")
@@ -41,3 +44,46 @@ class Game:
         print("Spock vaporizes Rock")
         print("")
         print("")
+    
+    def determine_player_count(self):
+        user_input = input('How many players will be playing? [Enter 1, 2, or 3 for a suprise]: ')
+        return user_input
+    
+    def player_move_choice(self):
+        pass
+    
+    def random_integer(self):
+        rand_int = Random.randint(0,4)
+        return rand_int
+    
+    def determine_winner(self, player_1_choice, player_2_choice):
+        if player_1_choice == 'rock':
+            pass
+
+
+    def battle(self, player_1, player_2):
+        battle = True
+        while battle == True:
+            if player_1 == self.human_1:
+                print("")
+                print("Choose 0 for Rock")
+                print("Choose 1 for Paper")
+                print("Choose 2 for Scissors")
+                print("Choose 3 for Lizard")
+                print("Choose 4 for Spock")
+                user_input = input("Which move would you like to do Player 1?: ")
+                player_1_choice = gesture_list[user_input]
+            elif player_1 == self.ai_1:
+                player_1_choice = gesture_list[self.random_integer()]
+            if player_2 == self.human_2:
+                print("")
+                print("Choose 0 for Rock")
+                print("Choose 1 for Paper")
+                print("Choose 2 for Scissors")
+                print("Choose 3 for Lizard")
+                print("Choose 4 for Spock")
+                user_input_2 = input("Which move would you like to do Player 2?: ")
+                player_2_choice = gesture_list[user_input_2]
+            elif player_1 == self.ai_2:
+                player_2_choice = gesture_list[self.random_integer()]
+            
--- main.py
+++ main.py
@@ -5,6 +5,15 @@ game = Game()
 
 game.run_game()
 
+#How to play Rock, Paper, Scissors, Spock
 
+#The User will have a list of 5 choices to pick from [Rock, Paper, Scissors, Lizards, Spock]
 
+#The AI will make a random choice based on a random index number to pick from the list
+
+#A battle phase should initiate between choices, in which a sequence of if statements occurs to determine the winner
+
+#The winner should then be displayed in a winner function
+
+#A while loop should be set up in run game function that will repeat the functions until the user intitiates for it to stop
 
