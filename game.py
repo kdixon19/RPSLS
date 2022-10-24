4e2531d62a361a08c93f10efc7e483ec949346f0
IN THE DOOR 2 <kristoffdixon19@gmail.com>
2022-10-24 00:23:00 -0400

    Final descriptive messages, code is done!

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from ai import AI
 import random
 
 class Game:
-    def __init__(self) -> None:
+    def __init__(self) -> None: #This function initializes our Game Class
         self.human_1 = Human('Player 1')
         self.human_2 = Human('Player 2')
         self.ai_1 = AI('AI 1')
@@ -11,7 +11,7 @@ class Game:
         global gesture_list
         gesture_list = ['rock','paper','scissors','lizard','spock']
 
-    def run_game(self):
+    def run_game(self): #This function runs the game for us
         game_run = True
         while game_run == True:
             self.display_welcome()
@@ -28,14 +28,14 @@ class Game:
         print('')
         print('Thanks for playing!')
 
-    def display_welcome(self):
+    def display_welcome(self): #This function displays the welcome message
         print("")
         print("Welcome to Rock Paper Scissors Lizards Spock")
         print("")
         print("Each mach will be best out of 3 games")
         print("Use the Number keys to enter your selection")
 
-    def display_rules(self):
+    def display_rules(self): #This function displays the rules
         print("")
         print("")
         print("Here are the rules:")
@@ -53,7 +53,7 @@ class Game:
         print("")
         print("")
     
-    def determine_player_count(self):
+    def determine_player_count(self): #This function determines the player count to determine which game mode is chosen
         entry_valid = False
         while entry_valid == False:
             user_input = input('How many players will be playing? [Enter 1, 2, or 3 for a suprise]: ')
@@ -62,11 +62,11 @@ class Game:
             else:
                 print('Im sorry, but that is not a valid entry')   
     
-    def random_integer(self):
+    def random_integer(self): #This function picks a random integer for the AI
         rand_int = random.randint(0,4)
         return rand_int
     
-    def round_winner(self, player_1_choice, player_2_choice):
+    def round_winner(self, player_1_choice, player_2_choice): #This function determines the winner for each round based on player choices
         if player_1_choice == 'rock':
             if player_2_choice == 'rock':
                 return 3
@@ -123,7 +123,7 @@ class Game:
             elif player_2_choice == 'spock':
                 return 3
 
-    def battle(self, player_1, player_2):
+    def battle(self, player_1, player_2): #This function tracks wins, the final winner, rounds, and conducts the battle phase
         round_counter = 0
         win_counter_1 = 0
         win_counter_2 = 0
@@ -167,7 +167,7 @@ class Game:
             round_counter += 1
         self.final_winner(win_counter_1,win_counter_2)
 
-    def player_choice(self, player):
+    def player_choice(self, player): #This validates the player choice to make sure it is a valid entry
         entry_valid = False
         while entry_valid == False:
             user_input = input(f'Which move would you like to choose {player.name}?: ')
@@ -176,7 +176,7 @@ class Game:
             else:
                 print('Im sorry, but that is not a valid entry')
 
-    def final_winner(self,win_counter_1, win_counter_2):
+    def final_winner(self,win_counter_1, win_counter_2): #This function determines the final winner
         if win_counter_1 > win_counter_2:
             print('Congratulations Player 1, you have won the game!')
         elif win_counter_1 < win_counter_2:
@@ -184,7 +184,7 @@ class Game:
         elif win_counter_1 == win_counter_2:
             print('We have a draw, you should play again!')
 
-    def determine_winner(self, winner):
+    def determine_winner(self, winner): #This detemines the winner based off what integer is passed in (1 for player 1, 2 for player 2, and 3 for tie)
         if winner == 1:
             print("")
             print('Player 1 has won!')
@@ -195,7 +195,7 @@ class Game:
             print('')
             print('Its a draw!')
 
-    def repeat_game(self):
+    def repeat_game(self): #This function repeats the game if the player chooses
         entry_valid = False
         while entry_valid == False:
             user_input = input('Would you like to play again? (y/n): ')
